2b5118f8afea90c821162a8e3352f84c591d2be7
Vinicius Damasceno <vinicius.poletti@ucsal.edu.br>
2025-05-20 15:10:35 -0300

    feat: Adiciona consulta SQL para buscar games por lista e projeção
    Implementa a funcionalidade de buscar games associados a uma lista específica.
    - Adicionada  ao  para buscar .
    - Utiliza  entre  e  para filtrar games por .
    - Mapeia os resultados para a interface , incluindo id, título, ano (gameYear), imgUrl, shortDescription e position.
    - Ordenação dos resultados pela posição na lista.

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -26,7 +26,7 @@ public class GameController {
 	}
 		
 	@GetMapping
-	public List<GameMinDTO> findall() {
+	public List<GameMinDTO> findAll() {
 		List<GameMinDTO> result = gameService.findAll();
 		return result;
 	}
